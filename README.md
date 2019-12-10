42dce29309cc3107ef4ac637b20c5352e7b310bd
LeonChenECE <38074191+LeonChenECE@users.noreply.github.com>
2019-12-10 21:41:06 +0100

    Correction Date

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Projet MERN (utilisant MongoDB, Express, React et Node.js)
 
 Utilisation de Recharts, Bootstrap, react-datepicker, react-router-dom, axios, mongoose.
 
+Malheuresuement, nous n'avons pas réussi à utiliser Storybook, ni créer des graphiques en utilisant les données du database. Voici donc des exemples de graphiques sur le dashboard.
+
 ## Instructions
 
 Pour démarer le site, 
@@ -13,6 +15,6 @@ Pour démarer le site,
 1) Ouvrir un onglet de terminal au dossier /backend et exécuter :
 nodemon server
 
-2) Ouvrir un onglet de terminal au dossier du projet et exécuter:
+2) Ouvrir un onglet de terminal au dossier /frontend et exécuter:
 npm start
 
---
+++ frontend/src/components/MeasureList.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Measure = props => (
+	<tr>
+		<td>{props.measure._id}</td>
+		<td>{props.measure.type}</td>
+		<td>{props.measure.creationDate}</td>
+		<td>{props.measure.value}</td>
+		<td>{props.measure.sensorID}</td>
+
+		<td>
+			<Link to={"/EditMeasure/"+props.measure._id}>Edit </Link>
+			 | 
+			<a href="#" onClick={() => { props.deleteMeasure(props.measure._id) }}> Delete</a>
+		</td>
+	</tr>
+
+	)
+
+export default class MeasureList extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.deleteMeasure = this.deleteMeasure.bind(this);
+
+		this.state = {measures: []};
+	}
+
+	componentDidMount(){
+		axios.get('http://localhost:5000/measure/')
+			.then(response => {
+				this.setState({ measures: response.data})
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+	}
+
+	deleteMeasure(id) {
+		axios.delete('http://localhost:5000/measure/'+id)
+		.then(res => console.log(res.data));
+
+		this.setState({
+			measures: this.state.measures.filter(el => el._id !== id)
+		})
+	}
+
+	measureList() {
+		return this.state.measures.map(currentmeasure => {
+			return <Measure measure={currentmeasure} deleteMeasure={this.deleteMeasure} key={currentmeasure._id}/>;
+		})
+	}
+
+	render() {
+		return(
+			<div>
+				<h3>List of all the Measures</h3>
+				<Link to={"/CreateMeasure"} className="btn btn-primary">Create a New Measure</Link>
+				<table className="table">
+					<thead className="thead-dark">
+						<tr>
+							<td>ID</td>
+
+							<td>Location</td>
+							<td>Creation Date</td>
+							<td>Value</td>
+							<td>Sensor ID</td>
+							<td>Actions</td>
+						</tr>
+					</thead>
+					<tbody>
+						{this.measureList()}
+					</tbody>
+				</table>
+			</div>
+			)
+	}
+
+}
